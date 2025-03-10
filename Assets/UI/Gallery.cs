4730da5415138d7e17a7730f3c888fb56fd3a87f
Ori-hub-code <uic3883@gmail.com>
2025-03-10 17:40:49 +0900

    갤러리 사진 클릭 기능 추가

--- Assets/UI/Gallery.cs
+++ Assets/UI/Gallery.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using static UnityEngine.GraphicsBuffer;
 
 public class Gallery : MonoBehaviour
@@ -9,17 +10,16 @@ public class Gallery : MonoBehaviour
     [SerializeField] private CellPhone cellPhone;
     private Vector2[][] imgAnchors;
     private Vector2[] bigAnchors;
-    [SerializeField] RectTransform[] imgRects;
+    [SerializeField] Image[] galleryImgs;
+    [SerializeField] RectTransform mainImgRect;
+    [SerializeField] GameObject bgImg;
 
     bool isSizeUpImg = false;
     int sizeUpImgIndex = -1;
-    [SerializeField] RectTransform bgImgRect;
 
     private void Awake()
     {
-        Vector3 newPosition = bgImgRect.position; // 현재 위치 가져오기
-        newPosition.z = 3; // Z 값 변경
-        bgImgRect.position = newPosition; // 변경된 위치 적용
+        SetFrist();
 
         imgAnchors = new Vector2[9][]
         {
@@ -39,11 +39,25 @@ public class Gallery : MonoBehaviour
         bigAnchors[1] = new Vector2(1f, 0.8f); // Max Value
     }
 
+    private void OnEnable()
+    {
+        SetFrist();
+    }
+
+    void SetFrist()
+    {
+        isSizeUpImg = false;
+        sizeUpImgIndex = -1;
+        bgImg.SetActive(false);
+        mainImgRect.anchorMax = Vector2.zero;
+        mainImgRect.anchorMin = Vector2.zero;
+    }
+
     public void BackButton()
     {
         if(isSizeUpImg && sizeUpImgIndex >= 0) // 사진이 커진 상태면 다시 작아지게
         {
-            StartCoroutine(AnchorsCoroutine(imgRects[sizeUpImgIndex], bigAnchors[0], bigAnchors[1], imgAnchors[sizeUpImgIndex][0], imgAnchors[sizeUpImgIndex][1], 0.15f, false));
+            StartCoroutine(AnchorsCoroutine(mainImgRect, bigAnchors[0], bigAnchors[1], imgAnchors[sizeUpImgIndex][0], imgAnchors[sizeUpImgIndex][1], 0.15f, false));
             isSizeUpImg = false;
             sizeUpImgIndex = -1;
         }
@@ -55,13 +69,9 @@ public class Gallery : MonoBehaviour
     
     public void HomeButton()
     {
-        if (isSizeUpImg && sizeUpImgIndex >= 0) // 사진이 커진 상태면 다시 작아지게
-        {
-            StartCoroutine(AnchorsCoroutine(imgRects[sizeUpImgIndex], bigAnchors[0], bigAnchors[1], imgAnchors[sizeUpImgIndex][1], imgAnchors[sizeUpImgIndex][1], 0.15f, false));
-            isSizeUpImg = false;
-            sizeUpImgIndex = -1;
-        }
-        cellPhone.HomeButton(this.GetComponent<RectTransform>());
+        cellPhone.HomeButton(this.GetComponent<RectTransform>()); // 갤러리 축소
+        isSizeUpImg = false;
+        sizeUpImgIndex = -1;
     }
 
     public void SizeUpImgButton(int index)
@@ -69,37 +79,23 @@ public class Gallery : MonoBehaviour
         if (isSizeUpImg)
             return;
 
-        StartCoroutine(AnchorsCoroutine(imgRects[index], imgAnchors[index][0], imgAnchors[index][1], bigAnchors[0], bigAnchors[1], 0.15f, true));
-        isSizeUpImg = true;
         sizeUpImgIndex = index;
+        isSizeUpImg = true;
+        StartCoroutine(AnchorsCoroutine(mainImgRect, imgAnchors[index][0], imgAnchors[index][1], bigAnchors[0], bigAnchors[1], 0.15f, true));
     }
 
     IEnumerator AnchorsCoroutine(RectTransform rectTransform, Vector2 startMin, Vector2 startMax, Vector2 targetMin, Vector2 targetMax, float time, bool sizeUp)
     {
-        // 검은색 배경 이미지 및 선택한 이미지 Pos Z 값 변경
         if (sizeUp)
         {
-            //// 검은색 배경
-            //Vector3 newPosition = bgImgRect.position; // 현재 위치 가져오기
-            //newPosition.z = -1; // Z 값 변경
-            //bgImgRect.position = newPosition; // 변경된 위치 적용
-
-            //// 선택한 이미지
-            //newPosition = rectTransform.position; // 현재 위치 가져오기
-            //newPosition.z = -2; // Z 값 변경
-            //rectTransform.position = newPosition; // 변경된 위치 적용
+            // Main Img 오브젝트 활성화
+            mainImgRect.gameObject.SetActive(true);
+            mainImgRect.GetComponent<Image>().sprite = galleryImgs[sizeUpImgIndex].sprite; // 이미지 변경
+            bgImg.SetActive(true);
         }
         else
         {
-            //// 검은색 배경
-            //Vector3 newPosition = bgImgRect.position; // 현재 위치 가져오기
-            //newPosition.z = 3; // Z 값 변경
-            //bgImgRect.position = newPosition; // 변경된 위치 적용
-
-            //// 선택한 이미지
-            //newPosition = rectTransform.position; // 현재 위치 가져오기
-            //newPosition.z = -1; // Z 값 변경
-            //rectTransform.position = newPosition; // 변경된 위치 적용
+            bgImg.SetActive(false);
         }
 
         float elapsed = 0f;
@@ -123,10 +119,8 @@ public class Gallery : MonoBehaviour
         // 선택한 이미지 크기 줄인 후 다시 Pos Z 값 0으로 초기화
         if (!sizeUp)
         {
-            //// 선택한 이미지
-            //Vector3 newPosition = rectTransform.position; // 현재 위치 가져오기
-            //newPosition.z = 0; // Z 값 변경
-            //rectTransform.position = newPosition; // 변경된 위치 적용
+            // Main Img 오브젝트 비활성화
+            mainImgRect.gameObject.SetActive(false);
         }
     }
 }
