5b45b6b25e086226501953f336b99c4a061cc54e
sun88110 <138852390+sun88110@users.noreply.github.com>
2025-03-17 17:29:31 +0900

    script

--- Assets/TheKiwiCoder/BehaviourTree/Scripts/Runtime/Blackboard.cs
+++ Assets/TheKiwiCoder/BehaviourTree/Scripts/Runtime/Blackboard.cs
@@ -3,13 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace TheKiwiCoder {
+namespace TheKiwiCoder
+{
 
     // This is the blackboard container shared between all nodes.
     // Use this to store temporary data that multiple nodes need read and write access to.
     // Add other properties here that make sense for your specific use case.
     [System.Serializable]
-    public class Blackboard {
+    public class Blackboard
+    {
         public Vector3 moveToPosition;
         public bool isDetected = false; // 적을 발견했는지 여부
 
