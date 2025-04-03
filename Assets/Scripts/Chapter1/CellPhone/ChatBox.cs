14a2f60bd3c43cb79eccba80889fd00358eef5af
Ori-hub-code <uic3883@gmail.com>
2025-04-03 19:17:01 +0900

    필요없는 코드 제거 및 초기 언어 설정 로직 추가

--- Assets/Scripts/Chapter1/CellPhone/ChatBox.cs
+++ Assets/Scripts/Chapter1/CellPhone/ChatBox.cs
@@ -12,7 +12,7 @@ using UnityEngine.UI;
 public class ChatBox : MonoBehaviour
 {
     public TextMeshProUGUI text; // Text UI
-    VerticalLayoutGroup verticalLayoutGroup;
+    VerticalLayoutGroup verticalLayoutGroup; // 언어 변경 시 chatBox 의 위치가 망가져서 강제 레이아웃 업데이트를 위해 가져옴.
     [SerializeField] bool changingLanguage;
 
     private void Awake()
@@ -23,19 +23,11 @@ public class ChatBox : MonoBehaviour
 
     void Start()
     {
-        StartCoroutine(ChangeLocalization(0)); // 영어 기본 세팅 {영어 : 0, 일본어 : 1, 한국어 : 2}
-    }
+        // 언어 변경
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[0];
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Keypad4))
-        {
-            ChangeLanguage(0); // 영어
-        }
-        else if (Input.GetKeyDown(KeyCode.Keypad5))
-        {
-            ChangeLanguage(2); // 한국어
-        }
+        // 강제로 레이아웃 업데이트
+        LayoutRebuilder.ForceRebuildLayoutImmediate(verticalLayoutGroup.GetComponent<RectTransform>());
     }
 
     public void ChangeLanguage(int index)
@@ -61,57 +53,9 @@ public class ChatBox : MonoBehaviour
         yield return LocalizationSettings.InitializationOperation;
         yield return new WaitForSeconds(0.5f);
 
-        // chatBox 정렬
-        if (this.gameObject.name.Contains("Other"))
-        {
-            verticalLayoutGroup.childAlignment = TextAnchor.UpperLeft; // 상대 채팅은 왼쪽에 배치
-            Debug.Log("Other");
-        }
-        else
-        {
-            verticalLayoutGroup.childAlignment = TextAnchor.UpperRight; // 내 채팅은 오른쪽에 배치
-        }
-
         // 강제로 레이아웃 업데이트
         LayoutRebuilder.ForceRebuildLayoutImmediate(verticalLayoutGroup.GetComponent<RectTransform>());
 
         changingLanguage = false;
-    }
-
-    // chatBox 크기 설정 및 글자 정렬
-    void AdjustText(int index)
-    {
-        // 텍스트를 띄어쓰기 기준으로 나누기
-        string[] words = text.text.Split(' ');
-        string formattedText = "";
-        int wordCount = 0;
-
-        // 줄바꿈 처리 (3개 단어마다 개행)
-        for (int i = 0; i < words.Length; i++)
-        {
-            if (wordCount != 3)
-            {
-                formattedText += words[i] + " ";
-                wordCount++;
-            }
-            else
-            {
-                formattedText += words[i] + "\n";
-                wordCount = 0;
-            }
-        }
-
-        // 텍스트 적용
-        text.text = formattedText.Trim();
-
-        if (this.gameObject.name.Contains("Other"))
-        {
-            verticalLayoutGroup.childAlignment = TextAnchor.UpperLeft; // 상대 채팅은 왼쪽에 배치
-            Debug.Log("Other");
-        }
-        else
-        {
-            verticalLayoutGroup.childAlignment = TextAnchor.UpperRight; // 내 채팅은 오른쪽에 배치
-        }
     }
 }
