10550645251e780646aa749b4ebb4dc8b358489b
Ori <uic3883@gmail.com>
2025-04-04 18:41:35 +0900

    휴대폰 canvas 분리 중

--- Assets/Scripts/Chapter1/CellPhone/PuzzleBoard.cs
+++ Assets/Scripts/Chapter1/CellPhone/PuzzleBoard.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
 using UnityEngine.UI;
 
 public class PuzzleBoard : MonoBehaviour
