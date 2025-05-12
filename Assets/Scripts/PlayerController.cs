6232c0153a01ded1d805bb90279ceb555131bdf1
Peinoi <ailis635@naver.com>
2025-05-12 14:48:34 +0900

    Scene PlayerTrSetting
    각 씬별로 플레이어 기본 위치값 세팅

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -80,7 +80,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
             m_Jumping = false;
             m_AudioSource = GetComponent<AudioSource>();
             m_MouseLook.Init(transform, m_Camera.transform);
-            if (!ProgressManager.Instance.progressData.newGame) { SpawnSet(); }
+            if (ProgressManager.Instance != null&&!ProgressManager.Instance.progressData.newGame) { SpawnSet(); }
 
         }
 
