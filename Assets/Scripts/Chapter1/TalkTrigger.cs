e160babeb3c140f373f670dff0460b49292e44e9
Yeongho <145641517+HongYeongHo@users.noreply.github.com>
2025-05-21 18:09:02 +0900

    hospital npc

---
+++ Assets/Scripts/Chapter1/TalkTrigger.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class TalkTrigger : MonoBehaviour
+{
+    public GameObject player;
+    public Supervisor npc;
+    public PlayableDirector timeline;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PlayerController.instance.Close_PlayerController(); // 플레이어 조작 멈춤
+            npc.StartWalkToPlayer(player.transform); // NPC 이동 시작
+            gameObject.SetActive(false); // 트리거 중복 방지
+        }
+    }
+}
